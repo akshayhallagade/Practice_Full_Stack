b9d8f173ca0b34b8ec541da96063153bf8bb9086
Akshay Hallagade <akshayhallagade2612@gmail.com>
2023-12-15 17:57:54 +0530

    update

---
+++ Full Stack LLD & Development 2/variables.js
@@ -0,0 +1,106 @@
+// ...................  Reassing in variables ..........
+// //....... 1. Let container.......
+// let name1 = "Akshay";
+// console.log(name1);
+
+// name1 = "Akshay Hallagade";
+// console.log(name1);
+
+// //....... 2. var container........
+// var name2 = "Akshay";
+// console.log(name2);
+
+// name2 = "Akshay Hallagade";
+// console.log(name2);
+
+// //....... 3. const container .......
+// const name3 = "Akshay";
+// console.log(name3);
+
+// name3 = "Akshay Hallagade";
+// console.log(name3);
+
+// ...................  Redeclare in variables ..........
+// // 1. Let container
+// let name1;
+// console.log(name1);
+
+// let name1;
+// console.log(name1);
+
+// // 2. var container
+// var name2 = "Akshay";
+// console.log(name2);
+
+// var name2;
+// console.log(name2);
+
+// // 3. const container
+// const name3 = "Akshay";
+// console.log(name3);
+
+// const name3 = "Askashay";
+// console.log(name3);
+
+// ...................  hosting in variables ..........
+// // 1. Let container
+// console.log(name1);
+// let name1="Akshay";
+
+// // 2. var container
+// console.log(name2);
+// var name2 = "Akshay";
+
+// // 3. const container
+// console.log(name3);
+// const name3 = "Akshay";
+
+// ...................  Limited scope of variables ..........
+// // 1. Let container -- Limited
+// {
+//   let name1 = "Akshay";
+//   console.log(name1);
+// }
+// console.log(name1);
+
+// // 2. var container -- Not Limited
+// {
+//   var name2 = "Akshay";
+//   console.log(name2);
+// }
+// console.log(name2);
+
+// 3. const container -- Limited
+// {
+//   const name2 = "Akshay";
+//   console.log(name2);
+// }
+// console.log(name2);
+
+// ----------------------------------------------------------------------
+// ............................... Variables.............................
+// ----------------------------------------------------------------------
+
+let name = "Akshay"; // string
+let age = 24; // number
+let yes = true; //boolean
+let grad; //undefined
+
+console.log(name, age, yes, grad);
+
+myArray = [
+  "Akshay",
+  24,
+  true,
+  undefined,
+  [1, 2, 3, 4, 5],
+  { grad: "Yes", pika: true },
+]; //Array
+obj = { grad: yes, school: true }; //obj
+
+console.log(myArray);
+console.log(obj);
+
+// ----------------------------------------------------------------------
+// ......................................................................
+// ----------------------------------------------------------------------
