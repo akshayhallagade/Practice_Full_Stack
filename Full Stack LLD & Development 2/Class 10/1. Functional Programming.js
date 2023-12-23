97d4b4622757b15f95dcdf087deac92c1cbaa75b
Akshay Hallagade <akshayhallagade2612@gmail.com>
2023-12-23 11:00:40 +0530

    lecture 10 added

---
+++ Full Stack LLD & Development 2/Class 10/1. Functional Programming.js
@@ -0,0 +1,9 @@
+// We can assing function to variable
+const sum = function (a, b) {
+  return a + b;
+};
+console.log(sum); // This is calling function reference
+console.log(sum(2, 3)); // This returning the function.
+
+// .....................................
+// A
