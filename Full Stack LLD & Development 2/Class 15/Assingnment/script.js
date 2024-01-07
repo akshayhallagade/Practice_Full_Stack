670c0bfd9ba394dec5531985c0f456259c68b1c7
Akshay Hallagade <akshayhallagade2612@gmail.com>
2024-01-07 15:04:46 +0530

    uodate lectures

---
+++ Full Stack LLD & Development 2/Class 15/Assingnment/script.js
@@ -0,0 +1,73 @@
+// WARNING: DO NOT EDIT THE BELOW CODE OR DATA
+
+// DUMMY DATA FOR THE COLORS
+let ticketsArr = [
+  {
+    ticketTask: "This is task 1 (lightgreen)",
+    ticketColor: "lightgreen",
+    ticketID: "dGSUFjfiq",
+  },
+  {
+    ticketTask: "This is task 2 (black)",
+    ticketColor: "black",
+    ticketID: "ay8dQS0o1",
+  },
+  {
+    ticketTask: "This is task 3 (lightblue)",
+    ticketColor: "lightblue",
+    ticketID: "fOqBFgQtx",
+  },
+  {
+    ticketTask: "This is task 4 (lightpink)",
+    ticketColor: "lightpink",
+    ticketID: "fOqBFgQtx",
+  },
+];
+
+// CALLING createTicket() function for each value in ticketsArr
+ticketsArr.forEach(function (ticket) {
+  createTicket(ticket.ticketTask, ticket.ticketColor, ticket.ticketID);
+});
+
+// ADDING TICKET TO DOM
+function createTicket(ticketTask, ticketColor, ticketID) {
+  let id = ticketID || shortid();
+  let ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
+
+  ticketCont.innerHTML = `<div class="ticket-color ${ticketColor}"></div>
+         <div class="ticket-id">${id}</div>
+         <div class="task-area"><textarea readonly="false">${ticketTask}</textarea></div>
+         <div class="ticket-lock">
+         <button>Locked</button>
+        </div>`;
+
+  let mainCont = document.querySelector(".main-cont");
+  mainCont.append(ticketCont);
+
+  if (!ticketID) {
+    ticketsArr.push({ ticketTask, ticketColor, ticketID: id });
+    localStorage.setItem("tickets", JSON.stringify(ticketsArr));
+  }
+}
+
+// Edited ..............................
+
+// Event Listener for all Buttons.
+const ticketLockRef = document.querySelectorAll(".ticket-lock button");
+ticketLockRef.forEach(function (lockRef) {
+  lockRef.addEventListener("click", function (event) {
+    const ticketContRef = lockRef.closest(".ticket-cont");
+    const textAreaRef = ticketContRef.querySelector("textarea");
+
+    if (event.target.innerText === "Locked") {
+      event.target.innerText = "Unlocked";
+      textAreaRef.removeAttribute("readonly");
+      lockRef.parentElement.classList.replace("ticket-lock", "ticket-unlock");
+    } else if (event.target.innerText === "Unlocked") {
+      event.target.innerText = "Locked";
+      textAreaRef.setAttribute("readonly", "");
+      lockRef.parentElement.classList.replace("ticket-unlock", "ticket-lock");
+    }
+  });
+});
--- Full Stack LLD & Development 2/Class 15/kanban board/script.js
+++ Full Stack LLD & Development 2/Class 15/kanban board/script.js
@@ -5,60 +5,47 @@ const modalRef = document.querySelector(".modal");
 const modalTextRef = document.querySelector(".modal .modal-left textarea");
 const selectCategoryRef = document.querySelectorAll(".modal-right .category");
 
-// Toggle Modal
+const tasks = [];
+
+// ............................................................
+// ..........................Home..............................
+// ............................................................
+// Toggle Modal View
 function toggleModal() {
   if (modalRef.classList.contains("hide")) {
     modalRef.classList.remove("hide");
-  } else {
+    categoryEvents();
     defaultSelectCategory();
+  } else {
     modalRef.classList.add("hide");
   }
 }
-//.................................................
 // Working of Create task Button
 addRef.addEventListener("click", function () {
   toggleModal();
 });
 
-const tasks = [];
-//.................................................
 // Creating New Card.
 function createCard(newTask) {
   const newCard = document.createElement("div");
   newCard.className = "card";
   newCard.innerHTML = `<div class="card-category ${newTask.Category}"></div><div class="card-id">${newTask.Id}</div><div class="card-detailes">${newTask.Details}</div><div class="card-delete"><i class="fa-solid fa-trash"></i></div>`;
-  cardsCollectionRef.appendChild(newCard);
-  tasks.push(newTask);
+  cardsCollectionRef.appendChild(newCard); // Adding newcard to DOM.
+  tasks.push(newTask); // Adding newTask to Array.
   console.log(tasks); //
+  deleteCard(newCard, newTask);
+}
+// Delete Card
+function deleteCard(newCard, newTask) {
   const deleteCardIconRef = newCard.querySelector(".fa-solid");
   deleteCardIconRef.addEventListener("click", function (event) {
-    deleteCardIconRef.parentElement.parentElement.remove();
+    deleteCardIconRef.parentElement.parentElement.remove(); //removing Card from DOM
     const selectedTaskRef = tasks.findIndex((task) => task.Id === newTask.Id);
-    tasks.splice(selectedTaskRef, 1);
+    tasks.splice(selectedTaskRef, 1); //removing Card from Array
     console.log(tasks); //
   });
 }
-//.................................................
-// Select Category
-selectCategoryRef.forEach(function (category) {
-  category.addEventListener("click", function (event) {
-    removeSelectedFromALL(); //removing selected categories
-    event.target.classList.add("selected");
-  });
-});
-// remove all selected categories
-function removeSelectedFromALL() {
-  selectCategoryRef.forEach(function (category) {
-    category.classList.remove("selected");
-  });
-}
-// Select Default Category
-defaultSelectCategory();
-function defaultSelectCategory() {
-  removeSelectedFromALL();
-  const defaultSelectRef = document.querySelector(".modal-right .c1.category");
-  defaultSelectRef.classList.add("selected");
-}
+// .........................................................
 
 // Add Card
 modalTextRef.addEventListener("keydown", function (event) {
@@ -67,15 +54,39 @@ modalTextRef.addEventListener("keydown", function (event) {
       ".modal-right .category.selected"
     );
     const dataAttributeRef = selectedRef.getAttribute("data-attribute");
+    // Creating Object
     const newTask = {
       Id: Math.random(),
       Details: event.target.value,
       Category: dataAttributeRef,
     };
-    createCard(newTask);
-    toggleModal();
-    event.target.value = "";
+    createCard(newTask); //based on the newTask object data CreateCard will create Card
+    toggleModal(); // hide the modal again
+    event.target.value = ""; //Setting value back to null for next Card Appear
   }
 });
 
-//delete
+//.................................................................
+// ...........................Modal................................
+//.................................................................
+// Select Category
+function categoryEvents() {
+  selectCategoryRef.forEach(function (category) {
+    category.addEventListener("click", function (event) {
+      removeSelectedFromALL(); //removing selected categories
+      event.target.classList.add("selected");
+    });
+  });
+}
+// remove all selected categories
+function removeSelectedFromALL() {
+  selectCategoryRef.forEach(function (category) {
+    category.classList.remove("selected");
+  });
+}
+// Select Default Category
+function defaultSelectCategory() {
+  removeSelectedFromALL();
+  const defaultSelectRef = document.querySelector(".modal-right .c1.category");
+  defaultSelectRef.classList.add("selected");
+}
