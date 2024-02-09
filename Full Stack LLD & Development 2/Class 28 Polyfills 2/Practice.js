a3ef7a363cc0d517cc18132f469d8764aa991e4f
Akshay Hallagade <akshayhallagade2612@gmail.com>
2024-02-09 23:18:44 +0530

    class 28

--- Full Stack LLD & Development 2/Class 28 Polyfills 2/Practice.js
+++ Full Stack LLD & Development 2/Class 28 Polyfills 2/Practice.js
@@ -1,12 +1,80 @@
-// console.log(x);
+// // Question 1
+// console.log(x); // error : referenceError
+//...........................
 
+// // Question 2
 // y = 1;
-// console.log(y);
+// console.log(y); // 1
 // var y;
+//...........................
 
-// y = 1;
-// console.log(y);
-// let y;
+// // Question 3
+// console.log(z); // undefined
+// var z=1;
+//...........................
+
+// // Question 4
+// a = 1;
+// console.log(a); // error : referenceError.
+// let a;
+//...........................
+
+// // Question 5
+// console.log(b);
+// let b = 1;
+//...........................
+
+// // Question 6
+// console.log(c); // Error : ReferenceError
+// c = 1;
+//...........................
+
+// // Question 7
+// console.log(sum); // undefined
+// var sum = function (a, b) {
+//   return a + b;
+// };
+
+//...........................
+
+// // Question 8
+// console.log(total); // error : referenceError
+// let total = function (a, b) {
+//   return a + b;
+// };
+
+//...........................
+
+// // Question 9
+// console.log(sum1(1, 2)); // 3
+// function sum1(a, b) {
+//   return a + b;
+// }
+//...........................
+
+// // Question 10
+// function example1() {
+//   if (true) {
+//     var aa = 1; // var is function scope
+//     let bb = 2; // let is block scope
+//   }
+//   console.log(aa); // 1
+//   console.log(bb); // Error : referenceError
+// }
+// example1();
+//...........................
+
+// // Question 11
+var cc = 20;
+function example1() {
+  if (true) {
+    var cc = 1; // var is function scope
+  }
+  console.log(cc); // 20
+}
+example1();
+console.log(cc); // 20
+//...........................
 
 // function parent2() {
 //   var a = 20;
