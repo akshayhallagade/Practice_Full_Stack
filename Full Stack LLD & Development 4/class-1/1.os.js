a55807fed19f2be958bc02e69014d5311df0c06a
Akshay Hallagade <akshayhallagade2612@gmail.com>
2024-03-27 10:27:12 +0530

    lecture 1 node js module added

---
+++ Full Stack LLD & Development 4/class-1/1.os.js
@@ -0,0 +1,9 @@
+const os = require("os");
+
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.machine());
+console.log(os.networkInterfaces());
+console.log(os.platform());
---
+++ Full Stack LLD & Development 4/class-1/4.path.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+// // Extension Name
+// console.log(
+//   path.extname(
+//     "F:S\tImportantAkshay's importantScalerPractice_Full_StackFull Stack LLD & Development 4class-1\3.fs-directory.js"
+//   )
+// );
+
+// // Basename
+// console.log(path.basename("class-1/3.fs-directory.js"));
+
+// // Directory Name
+console.log(__dirname);
+
+// // File Name
+console.log(__filename);
