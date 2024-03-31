38097bd5e2dcf8a4a53e40ac39891ab160e78668
Akshay Hallagade <akshayhallagade2612@gmail.com>
2024-03-31 13:19:06 +0530

    changes

--- Full Stack LLD & Development 4/class-1/1.os.js
+++
@@ -1,9 +0,0 @@
-const os = require("os");
-
-console.log(os.arch());
-console.log(os.cpus());
-console.log(os.homedir());
-console.log(os.hostname());
-console.log(os.machine());
-console.log(os.networkInterfaces());
-console.log(os.platform());
